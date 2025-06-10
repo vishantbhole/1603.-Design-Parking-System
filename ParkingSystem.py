4e766177427f31ff3fe59e0ae1d82144960b1d01
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-06-09 21:17:41 -0500

    Update ParkingSystem.py

--- ParkingSystem.py
+++ ParkingSystem.py
@@ -8,3 +8,19 @@ class ParkingSystem(object):
         self.big = big
         self.medium = medium
         self.small = small
+        
+    def addCar(self, carType):
+        """
+        :type carType: int
+        :rtype: bool
+        """
+        if carType == 1 and self.big > 0:
+            self.big = self.big - 1
+            return True
+        if carType == 2 and self.medium > 0:
+            self.medium -= 1
+            return True
+        if carType == 3 and self.small > 0:
+            self.small -= 1
+            return True
+        return False
